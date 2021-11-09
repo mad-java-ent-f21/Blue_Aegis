138a671471b9cb552d0964aeaf4f9ec93b24e92a
spartan7777 <saintvulcan@charter.net>
2021-11-09 15:10:45 -0600

    Updated files, added a new project into Team Repository.

---
+++ AegisHeroes/src/main/java/com/aegisheroes/persistence/SWAPIDao.java
@@ -0,0 +1,2 @@
+package com.aegisheroes.persistence;public class SWAPIDao {
+}
---
+++ AegisHeroes/src/test/java/com/aegisheroes/persistence/SWAPIDaoTest.java
@@ -0,0 +1,8 @@
+package com.aegisheroes.persistence;
+
+import junit.framework.TestCase;
+public class SWAPIDaoTest extends TestCase {
+
+    public void testGetPlanet() {
+    }
+}
