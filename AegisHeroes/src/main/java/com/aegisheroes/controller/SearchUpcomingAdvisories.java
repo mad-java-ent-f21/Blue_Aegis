b4966b3d6b06a3ac02f4e715fca3236f9a1e4af8
MichaeliRM30 <MichaeliRM30@gmail.com>
2021-11-14 18:44:05 -0600

    JSP Edits

--- AegisHeroes/src/main/java/com/aegisheroes/controller/SearchUpcomingAdvisories.java
+++ AegisHeroes/src/main/java/com/aegisheroes/controller/SearchUpcomingAdvisories.java
@@ -1,4 +1,37 @@
 package com.aegisheroes.controller;
 
-public class SearchUpcomingAdvisories {
+import com.aegisheroes.persistence.VulnerabilityDao;
+import com.aegisheroes.persistence.VulnerabilityData;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+/**
+ * A simple servlet to welcome the user.
+ * @author pwaite
+ */
+
+@WebServlet(
+        urlPatterns = {"/searchUsers"}
+)
+
+public class SearchUpcomingAdvisories extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //UserData userData = new UserData();
+        VulnerabilityDao vulnerabilityDaoDao = new VulnerabilityDao();
+        if (req.getParameter("submit").equals("search")) {
+            req.setAttribute("users", vulnerabilityDaoDao.getUsersByLastName(req.getParameter("searchTerm")));
+        } else {
+            req.setAttribute("users", vulnerabilityDaoDao.getAllUsers());
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
